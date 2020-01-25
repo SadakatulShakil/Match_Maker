84c69a249e6d2b8bbcca2aed61d50952dbbcd855
Sadakatul Ajam Md. Shakil <sadakatulshakil94@gmail.com>
2020-01-26 01:24:00 +0600

    SignUp SignIn design with TextInput layOut complete by Shakil

---
+++ app/src/main/java/com/example/matchmaker/Activity/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.matchmaker.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.matchmaker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSignUp, btnSignIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.signInPreview);
+        btnSignUp = findViewById(R.id.signUpPreview);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/matchmaker/Activity/SignUpActivity.java
@@ -0,0 +1,28 @@
+package com.example.matchmaker.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.matchmaker.R;
+
+public class SignUpActivity extends AppCompatActivity {
+    private ImageView backBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        backBtn = findViewById(R.id.arrow);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
